5a8c7afe7413cc0e8bb9971a8891a0f84b775c1d
alvachon <alvachon93@pm.me>
2022-12-13 20:47:42 -0500

    test from tuto CodeVault

---
+++ main.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvachon <alvachon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/13 20:21:54 by alvachon          #+#    #+#             */
+/*   Updated: 2022/12/13 20:46:43 by alvachon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int	main(int ac, char **av)
+{
+	int	fd[2];
+	int	pid1;
+	int	pid2;
+
+	if (pipe(fd) == -1)
+		return (1);
+	pid1 = fork();
+	if (pid1 < 0)
+		return (2);
+	if (pid1 == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("ping", "ping", "-c", "5", "google.com", NULL);
+	}
+	pid2 = fork();
+	if (pid2 < 0)
+		return (3);
+	if (pid2 == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("grep", "grep", "rtt", NULL);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid2, NULL, 0);
+	waitpid(pid1, NULL, 0);
+	return (0);
+}
